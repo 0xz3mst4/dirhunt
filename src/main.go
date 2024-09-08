ccc3248dc5598d5f4a3885d09964144eddc4a306
Mehdi <mehdifaiz42@outlook.com>
2024-09-08 19:41:18 +0200

    backup

--- src/main.go
+++ src/main.go
@@ -2,23 +2,35 @@ package main
 
 import (
 	// "io/ioutil"
+	"bufio"
 	"fmt"
-	"os"
 	"log"
 	"net/http"
+	"os"
 )
 
 func makeRequest(){
+	file, err := os.Open("/home/lists/big.txt")
+	defer file.Close()
 	args := os.Args
 
-	response, err := http.Get(args[1]);
+	r := bufio.NewReader(file)
+
+	for{
+		line, _, err := r.ReadLine()
+		if len(line) > 0{
+			response, err := http.Get(args[1] + line);
+
+		}
+	}
+
 	if err != nil{
 		log.Fatal(err)
 		return
 	}
-	defer response.Body.Close();
 	statusCode := response.StatusCode
 	fmt.Printf("Status Code: %d\n", statusCode)
+	defer response.Body.Close();
 }
 
 func main(){
